b4ee32655a9328f0ca7c89f2f6def1a567e031e5
Michael Xu <1378606230@qq.com>
2022-10-13 20:49:58 +0800

    Quicksort in Rust finished implemented

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,45 @@ fn quicksort(sort_vec: &mut Vec<i32>, begin: usize, end: usize) {
     // Get the index after partition, Now sort_vec[index] is just the correct pivot
     let index = partition(sort_vec, begin, end);
     // Sort the other two halves
-    quicksort(sort_vec, begin, index - 1);
+    if index != 0 { // Prevent the possible 'substract to overflow' problem
+        quicksort(sort_vec, begin, index - 1);
+    }
     quicksort(sort_vec, index + 1, end);
 }
 
 fn partition(sort_vec: &mut Vec<i32>, begin: usize, end: usize) -> usize {
-    // Here I choose middle element of the current vector as the pivot of quicksort
-    let middle = begin + (end - begin) / 2;
-    middle
+    // Here I choose the last element of the current vector as the pivot of quicksort
+    let last = sort_vec[end];
+    let mut ptr_for_smaller: i32 = match begin {
+        0 => -10,
+        _ => (begin - 1) as i32,
+    };
+    for i in begin..end {
+        if sort_vec[i] < last {
+            match ptr_for_smaller {
+                -10 => ptr_for_smaller = 0,
+                _ => ptr_for_smaller += 1,
+            }
+            // Swap the values in sort_vec[i] and sort_vec[ptr_for_smaller]
+            let tmp_val = sort_vec[ptr_for_smaller as usize];
+            sort_vec[ptr_for_smaller as usize] = sort_vec[i];
+            sort_vec[i] = tmp_val;
+        } else {
+           continue; 
+        }
+    }
+    
+    if ptr_for_smaller == -10 {
+        ptr_for_smaller = -1;
+    }
+
+    // Swap to make sure the pivot is in the right place
+    ptr_for_smaller += 1;
+    let tmp_val = sort_vec[ptr_for_smaller as usize];
+    sort_vec[ptr_for_smaller as usize] = sort_vec[end];
+    sort_vec[end] = tmp_val;
+
+    ptr_for_smaller as usize
 }
 
 fn read_file_lines(filename: &String) -> Result<Vec<String>, io::Error> {
